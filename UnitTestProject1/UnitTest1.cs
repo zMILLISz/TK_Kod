596ce671de2a5e8d06fcdc6c3ad6a4df4878471d
217086 <217086@KB37-1-120-C14>
2024-04-05 14:53:58 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WpfApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsTrue(window.Calculate("2", "5", "6"));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("", "", "6"));
+            Assert.IsFalse(window.Calculate("6", "", ""));
+            Assert.IsFalse(window.Calculate("", "6", ""));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("18", "20", "6.5"));
+            Assert.IsFalse(window.Calculate("18", "6.5", "2"));
+            Assert.IsFalse(window.Calculate("6.5", "20", "5"));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("erf", "24", "16"));
+            Assert.IsFalse(window.Calculate("13", "2fh", "16"));
+            Assert.IsFalse(window.Calculate("12", "24", "huhg"));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("3", "-3", "6"));
+            Assert.IsFalse(window.Calculate("3", "29", "-19"));
+            Assert.IsFalse(window.Calculate("-12", "7", "5"));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("&*^$", "6", "12"));
+            Assert.IsFalse(window.Calculate("7", "6", "&*^*("));
+            Assert.IsFalse(window.Calculate("16", "^%%$", "19"));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate(" ", " ", " "));
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            MainWindow window = new MainWindow();
+            Assert.IsFalse(window.Calculate("23", "4", "12"));
+            Assert.IsFalse(window.Calculate("15", "49", "3"));
+            Assert.IsFalse(window.Calculate("17", "6", "27"));
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool Calculate(string firstT, string secondT, string thirdT)
+        {
+            string firstModule = firstT;
+            string secondModule = secondT;
+            string thirdModule = thirdT;
+
+            string pattern = "^(0|[1-9]\\d*)$";
+            Regex regex = new Regex(pattern);
+
+            if (!(string.IsNullOrWhiteSpace(firstModule) && string.IsNullOrWhiteSpace(secondModule) && string.IsNullOrWhiteSpace(thirdModule)))
+            {
+                if (regex.IsMatch(firstModule) && regex.IsMatch(secondModule) && regex.IsMatch(thirdModule))
+                {
+                    int first = int.Parse(firstModule);
+                    int second = int.Parse(secondModule);
+                    int third = int.Parse(thirdModule);
+
+                    if (first <= 22 && second <= 38 && third <= 20)
+                    {
+                        int res = first + second + third;
+                        Summ.Text = $"{res}";
+
+                        if (res >= 0 && res <= 15) Result.Text = "2";
+                        else if (res >= 16 && res <= 31) Result.Text = "3";
+                        else if (res >= 32 && res <= 55) Result.Text = "4";
+                        else Result.Text = "5";
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Превышено максимальное количество баллов за модуль!");
+                        return false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Вводите только положительные целые числа!");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите баллы за все модули!");
+                return false;
+
+            }
+        }
+
+        private void CalculateResult_Click(object sender, RoutedEventArgs e)
+        {
+            Calculate(FirstModule.Text, SecondModule.Text, ThirdModule.Text);
+        }
+    }
+}
